e67129551d68570ad7eff9e063a60d038241d626
tamalsamanta <tamalsamanta2601@gmail.com>
2021-05-27 13:09:08 +0530

    reqd changes mentioned in comments

--- server.js
+++ server.js
@@ -2,13 +2,19 @@ const express = require('express')
 const app = express()
 const server = require('http').Server(app)
 const io = require('socket.io')(server)
-const { v4: uuidV4 } = require('uuid')
+const { v4: uuidV4 } = require('uuid') //dont really need to use this imo.
 
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-  res.redirect(`/${uuidV4()}`)
+  // res.redirect(`/${uuidV4()}`) --webDev Said so.
+  res.redirect('/default')
+  
+  
+  //This also works for custom room IDs. That can be used for creating multiple separate rooms n selective entry.
+  //Maybe later... use body parser to get the room IDs from the user login page to send them to the required room directly instead of 
+  //redirecting them to the default routes.
 })
 
 app.get('/:room', (req, res) => {
